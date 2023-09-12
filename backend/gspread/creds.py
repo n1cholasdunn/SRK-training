3b7de67bf3d211751f93d4b3f71a18c99ff91a7a
Nicholas J Dunn <nicholasduunn@gmail.com>
2023-09-11 20:55:33 -0700

    feat: gspread api fetching from training plans with service accnt

---
+++ backend/gspread/creds.py
@@ -0,0 +1,24 @@
+from decouple import config
+
+
+PROJECT_ID = config("GOOGLE_PROJECT_ID")
+PRIVATE_KEY_ID = config("SERVICE_PRIVATE_KEY_ID")
+PRIVATE_KEY = config("SERVICE_PRIVATE_KEY").replace(r"\n", "\n")
+CLIENT_EMAIL = config("SERVICE_CLIENT_EMAIL")
+CLIENT_ID = config("SERVICE_CLIENT_ID")
+CLIENT_X509_CERT_URL = config("GOOGLE_CLIENT_X509_CERT_URL")
+
+
+credentials = {
+    "type": "service_account",
+    "project_id": PROJECT_ID,
+    "private_key_id": PRIVATE_KEY_ID,
+    "private_key": PRIVATE_KEY,
+    "client_email": CLIENT_EMAIL,
+    "client_id": CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": CLIENT_X509_CERT_URL,
+    "universe_domain": "googleapis.com",
+}
---
+++ backend/gspread/get_training.py
@@ -0,0 +1,79 @@
+import gspread
+
+from creds import credentials
+
+gc = gspread.service_account_from_dict(credentials)
+
+sh = gc.open_by_url(
+    "https://docs.google.com/spreadsheets/d/16eR5pzMGubEou4c5-JBm86zoqbrco71ZlGrX-HZG6ig/edit?usp=sharing"
+)
+
+worksheet = sh.get_worksheet(1)
+
+workout_info = worksheet.get("B11:L25")
+
+
+def remove_empty_elements(data):
+    # Find the minimum sublist length
+    min_length = min(len(sublist) for sublist in data)
+
+    # Remove empty strings at the same index in all sublists
+    filtered_data = [[item for item in sublist if item != ""] for sublist in data]
+
+    # Truncate sublists to the minimum length
+    filtered_data = [sublist[:min_length] for sublist in filtered_data]
+
+    return filtered_data
+
+
+# Example data
+data = [
+    [
+        "Feet Only Slab/Weight Transfers",
+        "slab wall",
+        "1-2 min",
+        "3",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "No hands or only 1 finger for balance. Do NOT pull with hands at all. Shift weight over one and push with your legs. ",
+    ],
+    [
+        "Quiet Feet",
+        "any wall",
+        "1-2 min",
+        "3",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "Climb a route up and down with intentional foot placements and do NOT let your feet make a sound when placing them on a hold. Be light on your toes but fully weighting them. ",
+    ],
+    [
+        "Flagging Drills: Step throughs",
+        "vertical wall",
+        "1-2 min",
+        "3",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "Choose a climb that zig zags and allows you to practice stepping through to flag repeatedly. Focus on keeping your hips open and centered over your standing foot/ balanced between the opposite leg and same arm as standing foot side. Be intentional with your toe placements. ",
+    ],
+]
+
+# Remove empty elements
+filtered_data = remove_empty_elements(data)
+
+# Print the filtered data
+for sublist in filtered_data:
+    print(sublist)
+
+# print(workout_info)
